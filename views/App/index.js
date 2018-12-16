d4f5acea72e485ccd685b776abc0561497066461
weiqiang <mushangwq@163.com>
2018-12-16 11:15:56 +0800

    init the pro

---
+++ views/App/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './index.less';
+export default class APP extends Component {
+  render() {
+    return (
+      <div className=''>
+
+      </div>
+    );
+  }
+}
---
+++ views/index.js
@@ -0,0 +1,8 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import APP from './App/index';
+ReactDOM.render(
+  APP,
+  document.getElementById('app')
+);
